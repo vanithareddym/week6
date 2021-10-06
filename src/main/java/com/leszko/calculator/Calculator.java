1cbc50933b3b16cc54ba9697cf961c1aadd2dd59
vanithareddym <41925556+Vanithareddy3@users.noreply.github.com>
2021-10-06 17:10:17 -0400

    Change calculator java file

--- src/main/java/com/leszko/calculator/Calculator.java
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -3,11 +3,26 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+ * The type Calculator.
+ */
 @Service
 public class Calculator {
-        final static int umlNUMBER1 = 3;
-	@Cacheable("sum")
-	public int sum(int a, int b) {
-		return a + b;
-	}
+   /**
+    * The constant umlNUMBER1.
+    */
+   final static int umlNUMBER1 = 3;
+
+   /**
+    * Sum int.
+    *
+    * @param a the a
+    * @param b the b
+    * @return the int
+    */
+   @Cacheable("sum")
+   public int sum(int a, int b) {
+      return a + b;
+   }
 }
+
